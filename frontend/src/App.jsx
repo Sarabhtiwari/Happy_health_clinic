70384e0171a028e1bdd68227823acff92a23ae57
Krishna <kc180904@gmail.com>
2026-03-22 22:35:30 +0530

    Feat: added Homepage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import HomePage from './Components/HomePage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return (
+    <>
+     <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+     </Router>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function homePage() {
+  return (
+    <div>
+        <h1>Happy health clinic: Welcome</h1>
+        <nav>
+            <button>
+                Doctor
+            </button>
+        </nav>
+    </div>
+  )
+}
+
+export default homePage
