e92c4e8d77efdca029c150a898bf3ff2fe1573b0
Brimhesh Tiwari <tiwaribrimhesh@gmail.com>
2026-05-13 09:34:40 +0530

    Some change for otp problem

--- backend/services/otp.service.js
+++ backend/services/otp.service.js
@@ -2,10 +2,12 @@ const OtpVerification = require("../models/otpVerification.model");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS, 
+    pass: process.env.MAIL_PASS,
   },
 });
 
